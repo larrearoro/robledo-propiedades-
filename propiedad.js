cc4ee690b0c39627988bf99960b59e0db341d359
larrearoro <larrearoro@gmail.com>
2026-08-19 11:38:45 -0300

    Update propiedad.js

--- propiedad.js
+++ propiedad.js
@@ -8,7 +8,7 @@ console.error("No se encontró el ID de la propiedad");
 return;
 }
 
-const { data, error } = await supabaseClient
+const { data: propiedad, error } = await supabaseClient
 .from("propiedades")
 .select("*")
 .eq("id", id)
@@ -19,7 +19,38 @@ console.error("Error al cargar propiedad:", error);
 return;
 }
 
-console.log("PROPIEDAD:", data);
+console.log("PROPIEDAD:", propiedad);
+
+document.querySelector(".info-propiedad h1").textContent =
+propiedad.titulo;
+
+document.querySelector(".precio").textContent =
+propiedad.precio;
+
+document.querySelector(".ubicacion").textContent =
+"📍 " + propiedad.ubicacion;
+
+const caracteristicas =
+document.querySelectorAll(".caracteristicas div");
+
+caracteristicas[0].querySelector("strong").textContent =
+propiedad.dormitorios;
+
+caracteristicas[1].querySelector("strong").textContent =
+propiedad.banos;
+
+caracteristicas[2].querySelector("strong").textContent =
+propiedad.superficie + " m²";
+
+caracteristicas[3].querySelector("strong").textContent =
+propiedad.cochera;
+
+const fotoPrincipal =
+document.querySelector(".foto-principal");
+
+fotoPrincipal.innerHTML = `
+<img src="${propiedad.image_url}" alt="${propiedad.titulo}">
+`;
 }
 
 cargarPropiedad();
