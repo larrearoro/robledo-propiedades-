afbc182e58956aa05feaaf75980643315c3de75c
larrearoro <larrearoro@gmail.com>
2026-09-07 15:27:59 -0300

    Actualizar propiedades.js

--- propiedades.js
+++ propiedades.js
@@ -66,7 +66,7 @@ container.innerHTML += `
 <article class="propiedad-card">
 
 <div class="imagen-propiedad">
-<img src="${String(propiedad.image_url).trim()}" alt="${propiedad.titulo}">
+<img src="https://hkqxtatgwfqdqqwankxw.supabase.co/storage/v1/object/public/propiedades/3bis%20e/ab92ffd4-9767-4dea-aea3-506d8010d0c1.jpeg" alt="Prueba">
 </div>
 
 <div class="contenido-propiedad">
