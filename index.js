8d94c792eb53805a5ca1b45cdbb4a487c25d422b
larrearoro <larrearoro@gmail.com>
2026-09-08 21:45:19 -0300

    Actualizar index.js

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ container.innerHTML += `
 <article class="propiedad-card">
 
 <div class="imagen-propiedad">
-<img src="${String(propiedad.image_url).trim().replace("$0", "")}" alt="${propiedad.titulo}">
-</div>
+<img src="${String(propiedad.image_portada).trim().replace("$0", "")}" alt="${propiedad.titulo}">
 
 <h3>
 ${propiedad.titulo}
